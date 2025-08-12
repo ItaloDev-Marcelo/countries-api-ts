6859c62d50ee925bc3ea6742d3c60fdd9687244f
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-08-12 16:03:20 -0300

    alterando icons

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,11 +1,11 @@
 import Button from "./Button";
-import { IoIosArrowDown } from "react-icons/io";
+import { RiArrowDropDownLine } from "react-icons/ri";
 
 interface DropdownProps {handleSelect: (value: string) => void}
 const Dropdown = ({handleSelect}:DropdownProps ) => {
   return (
     <details className="dropdown w-50">
-      <summary className="btn my-1 w-50 bg-base-100 flex flex-row justify-between">Filter by Region < IoIosArrowDown size={15}/> </summary>
+      <summary className="btn my-1 w-50 bg-base-100 flex flex-row justify-between">Filter by Region <RiArrowDropDownLine size={15}/> </summary>
       <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-50 p-2 shadow-sm">
         <li><Button name="Africa"  handleSelect={handleSelect}/></li>
         <li><Button name="Americas" handleSelect={handleSelect} /></li>
--- src/components/ISearch.tsx
+++ src/components/ISearch.tsx
@@ -1,9 +1,9 @@
-import { IoMdSearch } from "react-icons/io";
+import { FaSearch } from "react-icons/fa";
 interface FormProps  {handleSearch: (e: React.ChangeEvent<HTMLInputElement>) => void}
 const ISearch = ({handleSearch}:FormProps) => {
   return (
     <div className='flex flex-row rounded-[5px] items-center mb-5 md:mb-0 bg-gray-200 w-90 md:w-100 h-[45px] p-3'>
-       <div className='pr-2' ><IoMdSearch size={30} /></div>
+       <div className='pr-2' >< FaSearch  size={30} /></div>
         <input type='text' name='search' placeholder='Search for a country...' onChange={handleSearch} />
     </div>
   )
