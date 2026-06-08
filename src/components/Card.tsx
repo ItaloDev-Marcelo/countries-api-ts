3fce1381784b918526b0281f0c94be601f0a196e
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-06-08 14:54:57 -0300

    style: add more style and add cursor-pointer

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,8 +5,8 @@ const Card = ({name, population,region,capital,img,id,handleDatails, toggle}:Car
 
 
   return (
-   <div onClick={() =>  handleDatails(id)} className={`card   w-[97%] md:w-[370px] tb:w-[445px] 
-    xl:w-[370px]  nt:w-75 m-2 shadow  transition duration-500 ease-in-out ${toggle ? 'bg-Blue-900 text-white ' : 'bg-Grey-50 text-Grey-950'}`} key={id}>
+   <div onClick={() =>  handleDatails(id)} className={`card cursor-pointer  w-[97%] md:w-[370px] tb:w-[445px] 
+    xl:w-[275px]  m-2 shadow  transition duration-500 ease-in-out ${toggle ? 'bg-Blue-900 text-white ' : 'bg-Grey-50 text-Grey-950'}`} key={id}>
   <figure>
     <img
       src={img}
@@ -14,7 +14,7 @@ const Card = ({name, population,region,capital,img,id,handleDatails, toggle}:Car
       className='w-full h-[200px]'
       />
   </figure>
-  <div className="card-body h-[250px] nt:h-[200px]">
+  <div className="card-body h-[250px]  nt:h-[200px]">
     <h2 className="card-title font-black">{name}</h2>
     <div className="mt-3">
       <p className="my-2 text-[14px]"> <strong>Population</strong>: {population}  </p>
