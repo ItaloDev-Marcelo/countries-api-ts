20868451461be2902876c63581577b2d6a0e61b1
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-06-08 14:57:54 -0300

    refactor: add more style and using map to render my component

--- src/Form/Dropdown.tsx
+++ src/Form/Dropdown.tsx
@@ -3,15 +3,19 @@ import { RiArrowDropDownLine } from "react-icons/ri";
 import type { DropdownProps } from "../types/DropdownType";
 
 const Dropdown = ({handleSelect, toggle}:DropdownProps ) => {
+
+  const countries = ['Africa','Americas','Asia','Europe','Oceania']
+
   return (
-    <details className="dropdown w-50">
-      <summary className={`btn my-1 w-50 border-0 outline-0 flex flex-row shadow justify-between ${toggle ? 'bg-Blue-900 text-Grey-50' : 'bg-White-zero text-Grey-950 '} `}>Filter by Region <RiArrowDropDownLine size={15}/> </summary>
+    <details className="dropdown w-50 shadow-2">
+      <summary className={`btn my-1 w-50 lg:h-[60px] border-0 outline-0 flex flex-row shadow font-normal  justify-between ${toggle ? 'bg-Blue-900 text-Grey-50' :
+         'bg-White-zero text-black '} `}>Filter by Region <RiArrowDropDownLine size={20} /> </summary>
       <ul className={`menu dropdown-content rounded-box z-1 w-50 p-2 shadow mt-2 ${toggle ? 'bg-Blue-900 text-Grey-50' : 'bg-White-zero  text-Grey-950 '}`}>
-        <li><Button name="Africa"  handleSelect={handleSelect}/></li>
-        <li><Button name="Americas" handleSelect={handleSelect} /></li>
-        <li> <Button name="Asia" handleSelect={handleSelect} /></li>
-        <li><Button name="Europe" handleSelect={handleSelect} /></li>
-        <li><Button name="Oceania" handleSelect={handleSelect} /></li>
+        {
+          countries.map((countriesName, index) => (
+            <li key={index}><Button name={countriesName}  handleSelect={handleSelect}/></li>
+          ))
+        }
       </ul>
     </details>
   );
