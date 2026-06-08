e960b07fa73a1d7ac6f08dc32ebfecf2ca0d559d
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-06-08 15:10:28 -0300

    style: add role

--- src/Form/Dropdown.tsx
+++ src/Form/Dropdown.tsx
@@ -10,10 +10,10 @@ const Dropdown = ({handleSelect, toggle}:DropdownProps ) => {
     <details className="dropdown w-50 shadow-2">
       <summary className={`btn my-1 w-50 lg:h-[60px] border-0 outline-0 flex flex-row shadow font-normal  justify-between ${toggle ? 'bg-Blue-900 text-Grey-50' :
          'bg-White-zero text-black '} `}>Filter by Region <RiArrowDropDownLine size={20} /> </summary>
-      <ul className={`menu dropdown-content rounded-box z-1 w-50 p-2 shadow mt-2 ${toggle ? 'bg-Blue-900 text-Grey-50' : 'bg-White-zero  text-Grey-950 '}`}>
+      <ul className={`menu dropdown-content rounded-box z-1 w-50 p-2 shadow mt-2 ${toggle ? 'bg-Blue-900 text-Grey-50' : 'bg-White-zero  text-Grey-950 '}`} role='menu'>
         {
           countries.map((countriesName, index) => (
-            <li key={index}><Button name={countriesName}  handleSelect={handleSelect}/></li>
+            <li key={index} role='none'><Button name={countriesName}  handleSelect={handleSelect}/></li>
           ))
         }
       </ul>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,7 @@
 import type { ButtonProps } from "../types/ButtonType"
 
 const Button = ({name, handleSelect}:ButtonProps) => {
-  return <button type='button' onClick={() => handleSelect(name)}>{name}</button>
+  return <button type='button' role='menuitem' onClick={() => handleSelect(name)}>{name}</button>
 }
 
 export default Button
