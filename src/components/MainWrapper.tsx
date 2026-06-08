a098ac5f62beb67084d49e5f65672ffd7dc5a53f
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-06-08 14:56:05 -0300

    refactor: change div to main and add style

--- src/components/MainWrapper.tsx
+++ src/components/MainWrapper.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 interface MainWrapperProps {children: React.ReactNode}
 const MainWrapper = ({children}:MainWrapperProps) => {
   return (
-    <div className='grid grid-col-12 items-center md:grid-cols-2 md:gap-5 lg:grid-cols-2 px-3
+    <main className='grid grid-col-12 items-center md:grid-cols-2 lg:my-19 md:gap-5 lg:grid-cols-2 px-3
      xl:grid-cols-3 nt:grid-cols-4  lg:gap-5 md:items-cente tb:gap-2.5 md:px-2 lg:px-9'>
         {children}
-    </div>
+    </main>
   )
 }
 
