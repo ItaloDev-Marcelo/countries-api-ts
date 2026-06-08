030bdaf941a5a1df8b3a3ffb588a409e139e35c8
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-06-08 15:02:27 -0300

    refactor: add type button to my button and more style

--- src/Header/ModeToggle.tsx
+++ src/Header/ModeToggle.tsx
@@ -3,7 +3,7 @@ import { FaRegMoon } from "react-icons/fa";
 type modeProps  = {DarkMode: () => void}
 
 const ModeToggle = ({DarkMode}:modeProps) => {
-  return  <button onClick={DarkMode} className='flex border-0 outline-0 p-1 font-semibold'> <FaRegMoon size={27}/> Dark Mode </button>
+  return  <button onClick={DarkMode} type='button' className='flex gap-2 cursor-pointer border-0 outline-0 p-1 font-semibold'> <FaRegMoon size={27}/> Dark Mode</button>
 }
 
 export default ModeToggle
