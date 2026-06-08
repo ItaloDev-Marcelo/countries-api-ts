091acabf2fb279d07841407b966a9fcb0e4de9e3
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-06-08 14:58:51 -0300

    style: add more style and add cursor-pointer

--- src/Form/ISearch.tsx
+++ src/Form/ISearch.tsx
@@ -2,11 +2,12 @@ import { FaSearch } from "react-icons/fa";
 import type { SearchProps } from "../types/SearchType";
 const ISearch = ({handleSearch, toggle}:SearchProps) => {
   return (
-    <div className={`flex flex-row rounded-[5px] items-center mb-5 shadow md:mb-0 transition duration-700 ease-in-out
-    w-full md:w-[50%] xl:w-[400px] h-[45px] p-3 ${toggle ? 'bg-Blue-900 text-Grey-50' : 'bg-White-zero text-Grey-950 '} `}>
-       <div className='pr-2' >< FaSearch  size={20} /></div>
-        <input type='text' name='search' placeholder='Search for a country...' onChange={handleSearch} className={toggle ? 'bg-Blue-900 text-White-zero transition duration-700 ease-in-ou' : 'bg-White-zero text-Grey-950  transition duration-700 ease-in-ou '} />
-   </div>
+    <div className={`flex flex-row  items-center mb-5 shadow md:mb-0 transition duration-700 ease-in-out gap-2
+    w-full md:w-[50%] xl:w-[400px] h-[45px] lg:h-[60px] p-3 ${toggle ? 'bg-Blue-900 text-Grey-50' : 'bg-White-zero text-Grey-950 '} `}>
+        <div className='pr-2' >< FaSearch  size={20} /></div>
+        <input type='text' role='search' name='search' placeholder='Search for a country...' onChange={handleSearch} className={`${toggle ? 'bg-Blue-900 text-White-zero transition duration-700 ease-in-ou' : 
+          'bg-White-zero text-Grey-950  transition duration-700 ease-in-ou '}`} />
+    </div>
   )
 }
 
--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -4,8 +4,9 @@ import ModeToggle from "./ModeToggle"
 const Header = ({DarkMode, toggle}:modeProps) => {
   
     return (
-    <header className={`flex flex-row justify-between p-4 lg:px-10 h-[75px] xl:h-[90px]  items-center transition duration-700 ease-in-out mb-4 ${toggle ? 'bg-Blue-950 text-white shadow-2xl ' : 'bg-White-zero text-Grey-950 shadow'}`}>
-       <a href='#index.html' className='font-black'><h1>Where in the world?</h1></a>
+    <header className={`flex flex-row justify-between lg:gap-10 p-4 lg:px-15  h-[75px] xl:h-[90px]  items-center transition duration-700 ease-in-out mb-4 ${toggle ? 'bg-Blue-950 text-white shadow-2xl ' : 'bg-White-zero text-Grey-950 shadow'}`}>
+      
+       <a href='#index.html'><span className='font-bold text-Grey-950 lg:text-2xl'>Where in the world?</span></a>
        <ModeToggle DarkMode={DarkMode}   />
     </header>
   )
